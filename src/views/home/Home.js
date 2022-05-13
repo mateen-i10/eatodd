ca636a509e76aba4d372b85ec5f839499d7e63c9
Mateen Sadaqat <mateen@vu360solutions.org>
2022-05-13 12:55:51 +0500

    fixed order

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -1,4 +1,3 @@
-import { Card, CardHeader, CardBody, CardTitle, CardText, CardLink } from 'reactstrap'
 import Order from "./components/Order/Order"
 import Hero from "./components/Hero/Hero"
 import Header from "../../shared/header/Header"
