84bd47d4bbe967f744200a34112ba7fad2906b66
Mateen Sadaqat <mateen@vu360solutions.org>
2022-05-13 12:52:45 +0500

    fixed some issues and styles

--- src/views/home/components/Hero/Hero.js
+++ src/views/home/components/Hero/Hero.js
@@ -1,15 +1,15 @@
 import React, {Component} from 'react'
 import "./Hero.css"
+import headervideo from '../../../../assets/videos/PolloAsado__omepageHero.mp4'
 
 
 class Hero extends Component {
     render() {
         return (
-
             <div className="hero-container">
                 <video   id="cmg-hero-video" autoPlay="autoplay" muted="muted"
                          loop="loop"
-                         src="https://www.chipotle.com/content/dam/chipotle/global-site-design/en/menu-items/pollo-asado/CHP_PolloAsado__omepageHero-DESKTOP.mp4"
+                         src={headervideo}
                          className="background-video" />
                 <div className="banner">
                     <div className="banner-content">
@@ -17,7 +17,6 @@ class Hero extends Component {
                         <div className="btn btn-danger hero-button">ORDER NOW</div>
                     </div>
                 </div>
-
             </div>
         )
     }
