3ab72ea7f7732d52a8cfc6f0a4df4f63e565ad0a
Talha Amjad <89654545+TalhaAmjad12@users.noreply.github.com>
2022-05-24 22:27:45 +0500

    miner changes.

--- src/router/routes/index.js
+++ src/router/routes/index.js
@@ -4,7 +4,7 @@ import { lazy } from 'react'
 const TemplateTitle = '%s - EatOMG'
 
 // ** Default Route
-const DefaultRoute = '/WineClub'
+const DefaultRoute = '/home'
 
 // ** Merge Routes
 const Routes = [
