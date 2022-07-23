62b3dcd2e5c56eedef3918d443944888278891b0
Talha Amjad <89654545+TalhaAmjad12@users.noreply.github.com>
2022-07-23 16:48:37 +0500

    Added the missing section and added half of the order pics.

--- src/views/home/components/Catering/Catering.js
+++ src/views/home/components/Catering/Catering.js
@@ -1,8 +1,48 @@
+import React from 'react'
+import MenuItemsCatering from "./MenuItemsCatering"
+import img from "../../../../assets/images/images/images.jpg"
+import {Col, Row} from "reactstrap"
+
 const Catering = () => {
-  return (
-      <div>
-          <h1>Catering tab</h1>
-      </div>
-  )
+    return (
+        <>
+            <div style={{backgroundColor:'white', paddingTop: 40}}>
+            <div className="text-center" style={{marginBottom: 30}}>
+                <h2 style={{color:'#81be41'}}>Popular Builds</h2>
+                <h4 className='my-2' style={{color:'#81be41'}}>Order the perfect crowd-pleaser spread for ant occasion</h4>
+                <h4 style={{color:'#81be41'}}>24 hours advance notice to order</h4>
+            </div>
+            <section>
+                <div className="container-sm">
+                    <div className="text-center">
+                        <Row>
+                            <Col md={3}>
+                                <div>
+                                    <MenuItemsCatering img={img} mealTittle="SIGNATURE" price='8' menu="Chiken Shawarma, Steak Shawarma, Falafel, Hummus, Mediterranean Salad, Turmeric Rice & OMG Sauces."/>
+                                </div>
+                            </Col>
+                            <Col md={3}>
+                                <div>
+                                    <MenuItemsCatering img={img} mealTittle="CLASSIC" price='5' menu="Chicken Kebob, Chicken Shawarma, Falafel, Hummus, Mediterranean Salad, Turmeric Rice & OMG Sauces." />
+                                </div>
+                            </Col>
+                            <Col md={3}>
+                                <div>
+                                    <MenuItemsCatering img={img} mealTittle="VEGETARIAN" price='5' menu="Falafel, Grape Leaves, Hummus, Baba Ghanouj, Persion Yogurt, Mediterranean Salad, Tabbouli Salad, Okra Stew, Rice & OMG Sauces." />
+                                </div>
+                            </Col>
+                            <Col md={3}>
+                                <div>
+                                    <MenuItemsCatering img={img} mealTittle="BOXED LUNCH" price='15' menu="Choice of Sandwich (Chicken Shawarma/ Chicken Kebob/ Steak Shawarma/ Falafel) Salad & Dessert." />
+                                </div>
+                            </Col>
+                        </Row>
+                    </div>
+                </div>
+            </section>
+            </div>
+        </>
+    )
 }
+
 export default Catering
