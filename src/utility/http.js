e72a60932ae53da9fd03fb45f26d3ad6dfbf7ae3
Syed Ali Naqi <syedalinaqi82@gmail.com>
2022-09-19 17:13:49 +0500

    Implemented API Related Configs

--- src/utility/http.js
+++ src/utility/http.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const baseURL = "https://localhost:7102/api/"
+export const baseURL = process.env.REACT_APP_API_BASEURL
 export default {
     _post: axios.post,
     _get: axios.get,
