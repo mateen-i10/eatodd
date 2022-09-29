493a74bde41042e731e813ce0509d5faf118ef74
MShahzad <intershezad@vu360solutions.org>
2022-09-29 15:57:49 +0500

    Order Menu page UI Changes done

--- src/views/home/options/components/TopShelf.js
+++ src/views/home/options/components/TopShelf.js
@@ -4,16 +4,17 @@ import ProductImage from "../../components/product/ProductImage"
 const TopShelf = ({attachment, name, description}) => {
     return (
         <>
-            <div className="container-sm">
-                <div className="row my-5">
+            <div className="container-sm ">
+                <div className="row mt-3">
                     <div className="col-md-5 col-12">
                         <ProductImage
                             attachment={attachment}
-                            styles={{width: "85%", height: "75%", borderRadius: 20}}/>
+                            styles={{width: "85%", height: "77%", borderRadius: 20}}/>
                     </div>
                     <div className="col-md-6 col-12 mt-1">
                         <div className='row'>
-                            <div className="col-sm-8 text-primary fw-bolder" style={{fontSize: "1.8rem", textTransform: 'uppercase'}}>
+                            <div className="col-sm-8 text-primary fw-bolder"
+                                 style={{fontSize: "1.8rem", textTransform: 'uppercase'}}>
                                 select your
                             </div>
                         </div>
