d5c14b59199753a51bcb6171372e8bf846cff17d
Talha Amjad <89654545+TalhaAmjad12@users.noreply.github.com>
2022-10-06 17:38:14 +0500

    worked on the API implementation of Members.

--- src/redux/member/reducer.js
+++ src/redux/member/reducer.js
@@ -1,45 +1,102 @@
 import {createAction} from '@reduxjs/toolkit'
+
 const initialState = {
     list: [],
     object: {},
+    miscData: {},
+    isRequestCompleted: false,
     isLoading: false,
-    isEdit: false
+    isEdit: false,
+    isError: false,
+    isSuccess: false
 }
 
-export const setCustomers = createAction("customerReducer/setCustomers")
-export const setCustomer = createAction("customerReducer/setCustomer")
-export const setLoading = createAction("customerReducer/setLoading")
-export const editCustomer = createAction("customerReducer/editCustomer")
+export const setMembers = createAction("memberReducer/setMembers")
+export const setMemberAddress = createAction("memberReducer/setMemberAddress")
+export const setMember = createAction("memberReducer/setMember")
+export const setLoading = createAction("memberReducer/setLoading")
+export const editMember = createAction("memberReducer/editMember")
+export const editMemberAddress = createAction("memberReducer/editMemberAddress")
+export const setDetailLoading = createAction("memberReducer/setDetailLoading")
+export const setIsEdit = createAction("memberReducer/setIsEdit")
+export const setIsMemberError = createAction("memberReducer/setIsMemberError")
+export const setIsMemberSuccess = createAction("memberReducer/setIsMemberSuccess")
+export const setRequestCompleted = createAction("memberReducer/setRequestCompleted")
 
-const customerReducer = (state = initialState, action) => {
+const memberReducer = (state = initialState, action) => {
     switch (action.type) {
-        case setCustomers.type:
+        case setMembers.type:
             return {
                 ...state,
-                list: action.payload,
+                list: action.payload.data,
+                miscData: action.payload.miscData,
                 isLoading: false,
-                isEdit: false
+                isDetailLoading: false,
+                isEdit: false,
+                isError: false,
+                isSuccess: false
             }
-        case setCustomer.type:
+        case setMember.type:
             return {
                 ...state,
-                object: action.payload,
-                isLoading: false
+                object: action.payload.data,
+                isEdit: false,
+                isLoading: false,
+                isDetailLoading: false
+            }
+        case setMemberAddress.type:
+            return {
+                ...state,
+                addressObject: action.payload.data,
+                isEdit: false,
+                isLoading: false,
+                isDetailLoading: false
             }
         case setLoading.type:
             return {
                 ...state,
                 isLoading: action.payload
             }
-        case editCustomer.type:
+        case setDetailLoading.type:
             return {
                 ...state,
-                object: action.payload,
+                isDetailLoading: action.payload
+            }
+        case editMember.type:
+            return {
+                ...state,
+                object: action.payload.data,
                 isEdit: true
+            }
+        case editMemberAddress.type:
+            return {
+                ...state,
+                addressObject: action.payload.data,
+                isEdit: true
+            }
+        case setRequestCompleted.type:
+            return {
+                ...state,
+                isRequestCompleted: action.payload
+            }
+        case setIsEdit.type:
+            return {
+                ...state,
+                isEdit: action.payload.data
+            }
+        case setIsMemberError.type:
+            return {
+                ...state,
+                isError: action.payload
+            }
+        case setIsMemberSuccess.type:
+            return {
+                ...state,
+                isSuccess: action.payload
             }
         default:
             return state
     }
 }
 
-export default customerReducer
+export default memberReducer
--- src/views/admin/WineClubMembers/Members.js
+++ src/views/admin/WineClubMembers/Members.js
@@ -1,216 +1,77 @@
 // ** React Imports
-import {Fragment, useEffect, useRef, useState} from 'react'
-
-// ** Form Modal Component for Add and Update
-import FormModal from '../../../components/FormModal'
+import React, {Fragment, useEffect, useState} from 'react'
 
 // ** Third Party Components
 import ReactPaginate from 'react-paginate'
 import DataTable from 'react-data-table-component'
-import Avatar from '../../../@core/components/avatar'
-import {ChevronDown, Edit, FileText, MoreVertical, Plus, Trash} from 'react-feather'
+import {ChevronDown, FileText} from 'react-feather'
 import {
     Card,
     CardHeader,
     CardTitle,
-    Button,
     Input,
-    Label,
     Row,
-    Col, Badge, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem
+    Col
 } from 'reactstrap'
-import {deleteCustomer, loadCustomers} from "../../../redux/member/actions"
+
 import {useDispatch, useSelector} from "react-redux"
-import {FieldTypes} from "../../../utility/enums/FieldType"
-import Joi from "joi-browser"
-import {setCustomer} from "../../../redux/member/reducer"
-import Swal from "sweetalert2"
+import UILoader from "../../../@core/components/ui-loader"
+import '@styles/react/libs/flatpickr/flatpickr.scss'
+import {loadMembers} from "../../../redux/member/actions"
+// import {getUserData} from "../../../auth/utils"
+// my changes
 
 const Members = () => {
-    const customerList = useSelector(state => state.member.list)
-    const formInitialState = useSelector(state => state.member.object)
-    const isEdit = useSelector(state => state.member.isEdit)
+
+    const memberList = useSelector(state => state.member.list)
+    const miscData = useSelector(state => state.member.miscData)
+    // const isLoading = useSelector(state => state.customer.isLoading)
     const dispatch = useDispatch()
 
+    // const customerId = getUserData().customerId
+    // console.log(customerId, "dataof user")
+
     // ** refs
-    const formModalRef = useRef(null)
-
-    // ** local States
-    const [currentPage, setCurrentPage] = useState(0)
+    const [currentPage, setCurrentPage] = useState(miscData && miscData.pageIndex ? miscData.pageIndex : 1)
+    const [pageSize] = useState(10)
     const [searchValue, setSearchValue] = useState('')
-    const [filteredData, setFilteredData] = useState([])
-    const [modalTitle] = useState('Add Customer')
-    const [formState, setFormState] = useState({})
-    const [isModal, setModal] = useState(false)
-    const [isModalLoading,  setModalLoading] = useState(false)
-    const [formData] = useState([
-        {type:FieldTypes.Text, label: 'Name', placeholder: 'Enter Name', name:'full_name', isRequired:true, fieldGroupClasses: 'col-6'},
-        {type:FieldTypes.Email, label: 'Price', placeholder: 'Enter Price', name:'price', isRequired:false, fieldGroupClasses: 'col-6'},
-        {type:FieldTypes.Email, label: 'Status', placeholder: 'Enter Status', name:'status', isRequired:false, fieldGroupClasses: 'col-6'}
-    ])
-
-    // ** schema for validations
-    const schema = Joi.object({
-        full_name: Joi.string().required().label("Name"),
-        email: Joi.string().required().label("Email")
-    })
-
 
     useEffect(() => {
-        dispatch(loadCustomers())
-        // in case of edit get item from backend
-        if (isEdit) setFormState({...formInitialState})
-        else {
-            dispatch(setCustomer({
-                full_name: '', email: ''
-            }))
-        }
-    }, [isEdit])
+        dispatch(loadMembers(currentPage, pageSize, searchValue))
+    }, [])
 
-    // ** Function to handle filter
-    const toggle = () => {
-        setModal(!isModal)
-        setFormState({...formInitialState})
-    }
-    // const addClick = () => {
-    //     setModalTitle('Add Customer')
-    //     toggle()
-    // }
-    const deleteClick = (id, e) => {
-        e.preventDefault()
-        // show sweet alert here
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                dispatch(deleteCustomer(id))
-            }
-        })
-    }
-    const handleSubmit = (event) => {
-        console.log("formState on submit", formState)
-        event.preventDefault()
-        const isError = formModalRef.current.validate(formState)
-        if (isError) return
-
-        // call api
-        setModalLoading(true)
-        console.log("form submitted")
-    }
     const handleFilter = e => {
+        console.log('e.keyCode', e.keyCode)
         const value = e.target.value
-        let updatedData = []
+        if (e.keyCode === 13) {
+            dispatch(loadMembers(currentPage + 1, pageSize, value))
+        }
         setSearchValue(value)
-
-        const status = {
-            1: { title: 'Current', color: 'light-primary' },
-            2: { title: 'Professional', color: 'light-success' },
-            3: { title: 'Rejected', color: 'light-danger' },
-            4: { title: 'Resigned', color: 'light-warning' },
-            5: { title: 'Applied', color: 'light-info' }
-        }
-
-        if (value.length) {
-            updatedData = data.filter(item => {
-                const startsWith =
-                    item.full_name.toLowerCase().startsWith(value.toLowerCase()) ||
-                    item.post.toLowerCase().startsWith(value.toLowerCase()) ||
-                    item.email.toLowerCase().startsWith(value.toLowerCase()) ||
-                    item.age.toLowerCase().startsWith(value.toLowerCase()) ||
-                    item.salary.toLowerCase().startsWith(value.toLowerCase()) ||
-                    item.start_date.toLowerCase().startsWith(value.toLowerCase()) ||
-                    status[item.status].title.toLowerCase().startsWith(value.toLowerCase())
-
-                const includes =
-                    item.full_name.toLowerCase().includes(value.toLowerCase()) ||
-                    item.post.toLowerCase().includes(value.toLowerCase()) ||
-                    item.email.toLowerCase().includes(value.toLowerCase()) ||
-                    item.age.toLowerCase().includes(value.toLowerCase()) ||
-                    item.salary.toLowerCase().includes(value.toLowerCase()) ||
-                    item.start_date.toLowerCase().includes(value.toLowerCase()) ||
-                    status[item.status].title.toLowerCase().includes(value.toLowerCase())
-
-                if (startsWith) {
-                    return startsWith
-                } else if (!startsWith && includes) {
-                    return includes
-                } else return null
-            })
-            setFilteredData(updatedData)
-            setSearchValue(value)
-        }
     }
 
     // ** Function to handle Pagination
     const handlePagination = page => {
-        setCurrentPage(page.selected)
+        dispatch(loadMembers(page.selected + 1, pageSize, searchValue))
+        setCurrentPage(page.selected + 1)
     }
 
-    // ** Table Common Column
-    const status = {
-        1: { title: 'Current', color: 'light-primary' },
-        2: { title: 'Professional', color: 'light-success' },
-        3: { title: 'Rejected', color: 'light-danger' },
-        4: { title: 'Resigned', color: 'light-warning' },
-        5: { title: 'Applied', color: 'light-info' }
-    }
+    // const detailOptClick = (id, e) => {
+    //     e.preventDefault()
+    //     props.history.push(`/customerDetails/${id}`)
+    // }
+
     const columns = [
         {
             name: 'Name',
-            selector: (row) => row.full_name,
+            selector: (row) => row.applicationUser.userName,
             sortable: true,
-            minWidth: '250px',
-            cell: row => (
-                <div className='d-flex align-items-center'>
-                    <div className='user-info text-truncate ms-1'>
-                        <span className='d-block font-weight-bold text-truncate'>{row.full_name}</span>
-                        <small>{row.post}</small>
-                    </div>
-                </div>
-            )
+            minWidth: '50px'
         },
         {
             name: 'Email',
-            selector: (row) => row.email,
+            selector: (row) => row.applicationUser.email,
             sortable: true,
-            minWidth: '250px'
-        },
-        {
-            name: 'Type',
-            selector: (row) => row.type,
-            sortable: true,
-            minWidth: '150px'
-        },
-        {
-            name: 'Status',
-            selector: (row) => row.status,
-            sortable: true,
-            minWidth: '150px',
-            cell: row => {
-                return (
-                    <Badge color={status[row.status].color} pill>
-                        {status[row.status].title}
-                    </Badge>
-                )
-            }
-        },
-        {
-            name: 'Period',
-            selector: (row) => row.age,
-            sortable: true,
-            minWidth: '100px'
-        },
-        {
-            name: 'Gift Membership',
-            selector: (row) => row.age,
-            sortable: true,
-            minWidth: '100px'
+            minWidth: '50px'
         },
         {
             name: 'Actions',
@@ -218,9 +79,7 @@ const Members = () => {
             cell: row => {
                 return (
                     <div className='d-flex'>
-                         <div tag='a' href='/' className='w-100' onClick={e => deleteClick(row.id, e)}>
-                             <Trash size={15} />
-                         </div>
+                        <span className='cursor-pointer mx-1' onClick={() => console.log(row.id)}><FileText size={15} /></span>
                     </div>
                 )
             }
@@ -228,75 +87,76 @@ const Members = () => {
     ]
 
     // ** Custom Pagination
-    const CustomPagination = () => (
-        <ReactPaginate
-            previousLabel=''
-            nextLabel=''
-            forcePage={currentPage}
-            onPageChange={page => handlePagination(page)}
-            pageCount={searchValue.length ? filteredData.length / 7 : customerList.length / 7 || 1}
+    const CustomPagination = () => {
+        const count = miscData?.totalPages ?? 0
+
+        return <ReactPaginate
+            previousLabel={''}
+            nextLabel={''}
             breakLabel='...'
-            pageRangeDisplayed={2}
+            pageCount={count || 1}
             marginPagesDisplayed={2}
+            pageRangeDisplayed={2}
             activeClassName='active'
-            pageClassName='page-item'
+            forcePage={currentPage !== 0 ? currentPage - 1 : 0}
+            onPageChange={page => handlePagination(page)}
+            pageClassName={'page-item'}
+            nextLinkClassName={'page-link'}
+            nextClassName={'page-item next'}
+            previousClassName={'page-item prev'}
+            previousLinkClassName={'page-link'}
+            pageLinkClassName={'page-link'}
             breakClassName='page-item'
             breakLinkClassName='page-link'
-            nextLinkClassName='page-link'
-            nextClassName='page-item next'
-            previousClassName='page-item prev'
-            previousLinkClassName='page-link'
-            pageLinkClassName='page-link'
-            containerClassName='pagination react-paginate separated-pagination pagination-sm justify-content-end pe-1 mt-1'
+            containerClassName={
+                'pagination react-paginate separated-pagination pagination-sm justify-content-end pe-1 mt-1'
+            }
         />
-    )
+    }
+
+    const dataToRender = () => {
+        if (memberList.length > 0) {
+            return memberList
+        }  else {
+            return memberList.slice(0, pageSize)
+        }
+    }
 
     return (
         <Fragment>
-            <Card>
-                <CardHeader className='flex-md-row flex-column align-md-items-center align-items-start border-bottom'>
-                    <CardTitle tag='h4'>Members</CardTitle>
-                    <div className='d-flex mt-md-0 mt-1'>
-                    </div>
-                </CardHeader>
-                <Row className='justify-content-end mx-0'>
-                    <Col className='d-flex align-items-center justify-content-end mt-1' md='12' sm='12'>
-                        <Input
-                            className='dataTable-filter mb-50'
-                            placeHolder="Search"
-                            type='text'
-                            bsSize='sm'
-                            id='search-input'
-                            value={searchValue}
-                            onChange={handleFilter}
-                        />
-                    </Col>
-                </Row>
-                <DataTable
-                    noHeader
-                    pagination
-                    columns={columns}
-                    paginationPerPage={7}
-                    className='react-dataTable'
-                    sortIcon={<ChevronDown size={10} />}
-                    paginationDefaultPage={currentPage + 1}
-                    paginationComponent={CustomPagination}
-                    data={searchValue.length ? filteredData : customerList}
-                />
-            </Card>
-            <FormModal ref={formModalRef}
-                       formState={formState}
-                       formData={formData}
-                       setFormState={setFormState}
-                       schema={schema}
-                       isModal={isModal}
-                       toggleModal={toggle}
-                       modalTitle={modalTitle}
-                       primaryBtnLabel='Save'
-                       secondaryBtnLabel='Cancel'
-                       isLoading = {isModalLoading}
-                       handleSubmit={handleSubmit}
-            />
+            <UILoader>
+                <Card>
+                    <CardHeader className='flex-md-row flex-column align-md-items-center align-items-start border-bottom'>
+                        <div>
+                            <CardTitle tag='h4'>Members</CardTitle>
+                            <h6>Friday June 10, 2022, 08:10 AM</h6>
+                        </div>
+                    </CardHeader>
+                    <Row className='justify-content-end mx-0'>
+                        <Col className='mt-1' md='12' sm='12'>
+                            <Input
+                                className='dataTable-filter mb-50'
+                                type='text'
+                                placeholder='Search'
+                                bsSize='sm'
+                                id='search-input'
+                                value={searchValue}
+                                onChange={handleFilter}
+                            />
+                        </Col>
+                    </Row>
+                    <DataTable
+                        noHeader
+                        pagination
+                        paginationServer
+                        className='react-dataTable'
+                        columns={columns}
+                        sortIcon={<ChevronDown size={10} />}
+                        paginationComponent={CustomPagination}
+                        data={dataToRender()}
+                    />
+                </Card>
+            </UILoader>
         </Fragment>
     )
 }
