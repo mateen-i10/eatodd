909bfb611254daf67d868b27ce8c55affbc40991
Talha Amjad <89654545+TalhaAmjad12@users.noreply.github.com>
2022-10-06 19:40:42 +0500

    catering customer API implementation done and some changes in member.

--- src/views/admin/Catering/CateringCutomers/CateringCustomers.js
+++ src/views/admin/Catering/CateringCutomers/CateringCustomers.js
@@ -1,154 +1,69 @@
 // ** React Imports
 import React, {Fragment, useEffect, useState} from 'react'
 
-
 // ** Third Party Components
 import ReactPaginate from 'react-paginate'
 import DataTable from 'react-data-table-component'
-import {ChevronDown, Edit, FileText, MoreVertical, Trash} from 'react-feather'
+import {ChevronDown, FileText} from 'react-feather'
 import {
-    Button,
     Card,
     CardHeader,
     CardTitle,
-    Col,
-    DropdownItem,
-    DropdownMenu,
-    DropdownToggle,
     Input,
     Row,
-    UncontrolledDropdown
+    Col
 } from 'reactstrap'
-import {deleteCrmSms, getCrmSms, loadCrmSms} from "../../../../redux/Catering/CateringCustomers/CatCustomersReducer"
-import {useDispatch, useSelector} from "react-redux"
-import Swal from "sweetalert2"
 
-const CateringCutomers = (props) => {
-    const customerList = useSelector(state => state.crmSms.list)
-    const isEdit = useSelector(state => state.crmSms.isEdit)
+import {useDispatch, useSelector} from "react-redux"
+import UILoader from "../../../../@core/components/ui-loader"
+import '@styles/react/libs/flatpickr/flatpickr.scss'
+import {loadCateringCustomerss} from "../../../../redux/Catering/CateringCustomers/actions"
+// import {getUserData} from "../../../auth/utils"
+// my changes
+
+const CateringCustomer = () => {
+
+    const cateringCustomerList = useSelector(state => state.customerCatering.list)
+    const miscData = useSelector(state => state.customerCatering.miscData)
+    // const isLoading = useSelector(state => state.customer.isLoading)
     const dispatch = useDispatch()
 
-    // ** local States
-    const [currentPage, setCurrentPage] = useState(0)
+    // ** refs
+    const [currentPage, setCurrentPage] = useState(miscData && miscData.pageIndex ? miscData.pageIndex : 1)
+    const [pageSize] = useState(10)
     const [searchValue, setSearchValue] = useState('')
-    const [filteredData, setFilteredData] = useState([])
-    const [isModal, setModal] = useState(false)
-    // const [editData, setEditData] = useState(0)
-
 
     useEffect(() => {
-        dispatch(loadCrmSms())
-    }, [isEdit])
+        dispatch(loadCateringCustomerss(currentPage, pageSize, searchValue))
+    }, [])
 
-    // ** Function to handle filter
-    const toggle = () => {
-        setModal(!isModal)
-    }
-    const addClick = (e) => {
-        e.preventDefault()
-        toggle()
-    }
-    const editClick = (data) => {
-        toggle()
-        dispatch(getCrmSms(data.id, true))
-        // setEditData(data)
-        console.log('id of the user', data.id, data.full_name)
-    }
-    const deleteClick = (id, e) => {
-        e.preventDefault()
-        // show sweet alert here
-        Swal.fire({
-            title: 'Are you sure?',
-            text: "You won't be able to revert this!",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Yes, delete it!'
-        }).then((result) => {
-            if (result.isConfirmed) {
-                dispatch(deleteCrmSms(id))
-            }
-        })
-    }
-    const detailOptClick = (id, e) => {
-        e.preventDefault()
-        console.log('call', props)
-        props.history.push(`/customers/detail/${id}`)
-    }
     const handleFilter = e => {
+        console.log('e.keyCode', e.keyCode)
         const value = e.target.value
-        let updatedData = []
+        if (e.keyCode === 13) {
+            dispatch(loadCateringCustomerss(currentPage + 1, pageSize, value))
+        }
         setSearchValue(value)
-
-        const status = {
-            1: {title: 'Current', color: 'light-primary'},
-            2: {title: 'Professional', color: 'light-success'},
-            3: {title: 'Rejected', color: 'light-danger'},
-            4: {title: 'Resigned', color: 'light-warning'},
-            5: {title: 'Applied', color: 'light-info'}
-        }
-
-        if (value.length) {
-            updatedData = data.filter(item => {
-                const startsWith =
-                    item.full_name.toLowerCase().startsWith(value.toLowerCase()) ||
-                    item.post.toLowerCase().startsWith(value.toLowerCase()) ||
-                    item.email.toLowerCase().startsWith(value.toLowerCase()) ||
-                    item.age.toLowerCase().startsWith(value.toLowerCase()) ||
-                    item.salary.toLowerCase().startsWith(value.toLowerCase()) ||
-                    item.start_date.toLowerCase().startsWith(value.toLowerCase()) ||
-                    status[item.status].title.toLowerCase().startsWith(value.toLowerCase())
-
-                const includes =
-                    item.full_name.toLowerCase().includes(value.toLowerCase()) ||
-                    item.post.toLowerCase().includes(value.toLowerCase()) ||
-                    item.email.toLowerCase().includes(value.toLowerCase()) ||
-                    item.age.toLowerCase().includes(value.toLowerCase()) ||
-                    item.salary.toLowerCase().includes(value.toLowerCase()) ||
-                    item.start_date.toLowerCase().includes(value.toLowerCase()) ||
-                    status[item.status].title.toLowerCase().includes(value.toLowerCase())
-
-                if (startsWith) {
-                    return startsWith
-                } else if (!startsWith && includes) {
-                    return includes
-                } else return null
-            })
-            setFilteredData(updatedData)
-            setSearchValue(value)
-        }
     }
 
     // ** Function to handle Pagination
     const handlePagination = page => {
-        setCurrentPage(page.selected)
+        dispatch(loadCateringCustomerss(page.selected + 1, pageSize, searchValue))
+        setCurrentPage(page.selected + 1)
     }
 
     const columns = [
         {
-            name: 'Schedule',
-            selector: (row) => row.schedule,
+            name: 'Name',
+            selector: (row) => row.applicationUser.userName,
             sortable: true,
             minWidth: '50px'
         },
         {
-            name: 'SMS',
-            selector: (row) => row.sms,
+            name: 'Email',
+            selector: (row) => row.applicationUser.email,
             sortable: true,
-            minWidth: '250px'
-        },
-        {
-            name: 'Filters',
-            selector: (row) => row.filter,
-            sortable: true,
-            minWidth: '150px'
-        },
-        {
-            name: 'Status',
-            selector: (row) => row.status,
-            sortable: true,
-            minWidth: '150px'
+            minWidth: '50px'
         },
         {
             name: 'Actions',
@@ -156,25 +71,7 @@ const CateringCutomers = (props) => {
             cell: row => {
                 return (
                     <div className='d-flex'>
-                        <UncontrolledDropdown>
-                            <DropdownToggle className='pe-1' tag='span'>
-                                <MoreVertical size={15}/>
-                            </DropdownToggle>
-                            <DropdownMenu end>
-                                <DropdownItem tag='a' href='/' className='w-100'
-                                              onClick={e => detailOptClick(row.id, e)}>
-                                    <FileText size={15}/>
-                                    <span className='align-middle ms-50'>Details</span>
-                                </DropdownItem>
-                                <DropdownItem tag='a' href='/' className='w-100' onClick={e => deleteClick(row.id, e)}>
-                                    <Trash size={15}/>
-                                    <span className='align-middle ms-50'>Delete</span>
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </UncontrolledDropdown>
-                        <span onClick={() => {
-                            editClick(row)
-                        }}><Edit size={15}/></span>
+                        <span className='cursor-pointer mx-1' onClick={() => console.log(row.id)}><FileText size={15} /></span>
                     </div>
                 )
             }
@@ -182,69 +79,78 @@ const CateringCutomers = (props) => {
     ]
 
     // ** Custom Pagination
-    const CustomPagination = () => (
-        <ReactPaginate
-            previousLabel=''
-            nextLabel=''
-            forcePage={currentPage}
-            onPageChange={page => handlePagination(page)}
-            pageCount={searchValue.length ? filteredData.length / 7 : customerList.length / 7 || 1}
+    const CustomPagination = () => {
+        const count = miscData?.totalPages ?? 0
+
+        return <ReactPaginate
+            previousLabel={''}
+            nextLabel={''}
             breakLabel='...'
-            pageRangeDisplayed={2}
+            pageCount={count || 1}
             marginPagesDisplayed={2}
+            pageRangeDisplayed={2}
             activeClassName='active'
-            pageClassName='page-item'
+            forcePage={currentPage !== 0 ? currentPage - 1 : 0}
+            onPageChange={page => handlePagination(page)}
+            pageClassName={'page-item'}
+            nextLinkClassName={'page-link'}
+            nextClassName={'page-item next'}
+            previousClassName={'page-item prev'}
+            previousLinkClassName={'page-link'}
+            pageLinkClassName={'page-link'}
             breakClassName='page-item'
             breakLinkClassName='page-link'
-            nextLinkClassName='page-link'
-            nextClassName='page-item next'
-            previousClassName='page-item prev'
-            previousLinkClassName='page-link'
-            pageLinkClassName='page-link'
-            containerClassName='pagination react-paginate separated-pagination pagination-sm justify-content-end pe-1 mt-1'
+            containerClassName={
+                'pagination react-paginate separated-pagination pagination-sm justify-content-end pe-1 mt-1'
+            }
         />
-    )
+    }
+
+    const dataToRender = () => {
+        if (cateringCustomerList.length > 0) {
+            return cateringCustomerList
+        }  else {
+            return cateringCustomerList.slice(0, pageSize)
+        }
+    }
 
     return (
         <Fragment>
-            <Card>
-                <CardHeader className='flex-md-row flex-column align-md-items-center align-items-start border-bottom'>
-                    <div>
-                        <CardTitle tag='h4'>CrmSms</CardTitle>
-                        <h6>Friday June 10, 2022, 08:10 AM</h6>
-                    </div>
-                    <Button.Ripple color='primary' onClick={(e) => addClick(e)}>Schedule a new SMS</Button.Ripple>
-                </CardHeader>
-                <Row className='mx-0'>
-                    <Col className='mt-1' md='12' sm='12'>
-                        <Input
-                            className='dataTable-filter mb-50'
-                            type='text'
-                            bsSize='sm'
-                            placeholder='search'
-                            id='search-input'
-                            value={searchValue}
-                            onChange={handleFilter}
-                        />
-                    </Col>
-                </Row>
-                <DataTable
-                    noHeader
-                    pagination
-                    columns={columns}
-                    paginationPerPage={7}
-                    className='react-dataTable'
-                    sortIcon={<ChevronDown size={10}/>}
-                    paginationDefaultPage={currentPage + 1}
-                    paginationComponent={CustomPagination}
-                    data={searchValue.length ? filteredData : customerList}
-                />
-            </Card>
-
-            {/*<ScheduleSms isShow={isModal} setShow={toggle} data={editData}/>*/}
-
+            <UILoader>
+                <Card>
+                    <CardHeader className='flex-md-row flex-column align-md-items-center align-items-start border-bottom'>
+                        <div>
+                            <CardTitle tag='h4'>Catering Customer</CardTitle>
+                            <h6>Friday June 10, 2022, 08:10 AM</h6>
+                        </div>
+                    </CardHeader>
+                    <Row className='justify-content-end mx-0'>
+                        <Col className='mt-1' md='12' sm='12'>
+                            <Input
+                                className='dataTable-filter mb-50'
+                                type='text'
+                                placeholder='Search'
+                                bsSize='sm'
+                                id='search-input'
+                                value={searchValue}
+                                onChange={handleFilter}
+                            />
+                        </Col>
+                    </Row>
+                    <DataTable
+                        noHeader
+                        pagination
+                        paginationServer
+                        className='react-dataTable'
+                        columns={columns}
+                        sortIcon={<ChevronDown size={10} />}
+                        paginationComponent={CustomPagination}
+                        data={dataToRender()}
+                    />
+                </Card>
+            </UILoader>
         </Fragment>
     )
 }
 
-export default CateringCutomers
+export default CateringCustomer
