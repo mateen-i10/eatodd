a12726bb8b0ca8da39afcade89678e31a10f7525
Hamza <hamzahabib201@gmail.com>
2022-11-01 13:24:23 +0500

    api hook updated

--- src/utility/customHooks/useAPI.js
+++ src/utility/customHooks/useAPI.js
@@ -6,24 +6,29 @@ const useAPI = (url, method, data, responseType, isErrorToast = false, isSuccess
     const [isLoading, setIsLoading] = useState(false)
     const [response, setResponse] = useState(null)
 
-    useEffect(async () => {
-        try {
+    useEffect(() => {
+        const controller = new AbortController()
             if (url && url !== '') {
                 setIsLoading(true)
-                const res =  await http._request({baseURL, url, method, data, responseType})
-                if (res && res.status === 200 && (res.data instanceof Blob || res.data.statusCode === 200)) {
-                    setIsLoading(false)
-                    setResponse(res.data)
-                    if (isSuccessToast) toast.success(res.data.message)
-                } else {
-                    if (isErrorToast) toast.error(res.data.message)
-                    setIsLoading(false)
-                }
-            }
+                 http._request({baseURL, url, method, data, responseType, signal: controller.signal })
+                    .then(res => {
+                        if (res && res.status === 200 && (res.data instanceof Blob || res.data.statusCode === 200)) {
+                            setIsLoading(false)
+                            setResponse(res.data)
+                            if (isSuccessToast) toast.success(res.data.message)
+                        } else {
+                            if (isErrorToast) toast.error(res.data.message)
+                            setIsLoading(false)
+                        }
+                    }
+                ).catch(e => {
+                         setIsLoading(false)
+                         if (isErrorToast) toast.error(e.message)
+                     })
 
-        } catch (e) {
-            setIsLoading(false)
-            if (isErrorToast) toast.error(e.message)
+            }
+        return () => {
+            controller.abort()
         }
 
     }, [url])
