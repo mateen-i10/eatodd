4ed849e9c3e846abb880e4c568ea06d06f749881
Talha Amjad <89654545+TalhaAmjad12@users.noreply.github.com>
2022-11-21 15:51:00 +0500

    applied validations on crm email template form.

--- src/views/admin/CrmEmails/components/CrmForm.js
+++ src/views/admin/CrmEmails/components/CrmForm.js
@@ -3,6 +3,8 @@ import {Button, Col, Input, Label, Modal, ModalBody, ModalFooter, ModalHeader, R
 import {useDispatch} from "react-redux"
 import {addTemplate} from "../../../../redux/template/action"
 
+import Joi from 'joi-browser'
+
 const CrmForm = (props) => {
 
     // states
@@ -11,7 +13,16 @@ const CrmForm = (props) => {
     const [body, setBody] = useState("")
 
     // bool
-    const [isHtml, setIsHtml] = useState(false)
+    const [isHtml, setIsHtml] = useState(true)
+
+    // Schema
+    const schema = Joi.object({
+        name: Joi.string().required().label("Name"),
+        body: Joi.string().required().label("Body")
+    })
+
+    //setting errors
+    const [errors, setErrors] = useState({})
 
     // dispatcher initialization
     const dispatch = useDispatch()
@@ -32,7 +43,10 @@ const CrmForm = (props) => {
     }
 
     const handleSubmit = () => {
-
+        const data = {
+            name,
+            body
+        }
         const finaldata = {
             name,
             isHtml,
@@ -40,11 +54,27 @@ const CrmForm = (props) => {
             body
         }
 
-        dispatch(addTemplate(finaldata))
-
-        handleClose()
-        props.setModal(!props.isModal)
+        const isError = Joi.validate(data, schema, {abortEarly: false})
+        console.log(isError, "errors")
+        const {error} = isError
+        if (!error) {
+            dispatch(addTemplate(finaldata))
+            handleClose()
+            props.setModal(!props.isModal)
+        } else {
+            const errorData = {}
+            for (const item of error?.details) {
+                console.log(item, "ITEM MSG")
+                const name = item.path[0]
+                const message = item.message
+                errorData[name] = message
+            }
+            setErrors(errorData)
+            // return errorData
+        }
     }
+
+    console.log(errors)
 
     return (
         <>
@@ -63,7 +93,12 @@ const CrmForm = (props) => {
                                 <Row>
                                     <Col lg={6} style={{marginTop:'10px', marginBottom:'10px'}}>
                                         <Label for="name" >Name</Label>
-                                        <Input type="text" name="name" value={name} onChange={(e) => setTempName(e.target.value)} placeholder="Enter Template Name" />
+                                        <Input type="text" name="name" value={name} onChange={(e) => setTempName(e.target.value)} placeholder="Enter Template Name" invalid={!name ? errors.name && true : null}/>
+                                        {!name ? errors.name && (
+                                            <div className="text-danger">
+                                                {errors.name}
+                                            </div>
+                                        ) : null}
                                     </Col>
                                     <Col lg={6} style={{marginTop:'10px', marginBottom:'10px'}}>
                                         <Label for="html">IsHtml</Label>
@@ -77,7 +112,12 @@ const CrmForm = (props) => {
                                     </Col> : [] }
                                     <Col lg={6} style={{marginTop:'10px', marginBottom:'10px'}}>
                                         <Label for="body" >body</Label>
-                                        <Input type="textarea" name='body' value={body} onChange={(e) => setBody(e.target.value)} placeholder="Enter Template Body" />
+                                        <Input type="textarea" name='body' value={body} onChange={(e) => setBody(e.target.value)} placeholder="Enter Template Body" invalid={!body ? errors.body && true : null} />
+                                        {!body ? errors.body && (
+                                            <div className="text-danger">
+                                                {errors.body}
+                                            </div>
+                                        ) : null}
                                     </Col>
                                 </Row>
                             </div>
