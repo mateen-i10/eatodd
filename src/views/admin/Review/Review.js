30f1b8301e22dcf72a9235f957d48f15ff3adecb
zabair <zabair1146@gmail.com>
2022-12-05 18:59:39 +0500

    review crud working changes

--- src/views/admin/Review/Review.js
+++ src/views/admin/Review/Review.js
@@ -49,11 +49,11 @@ const userReview = (props) => {
     const [searchValue, setSearchValue] = useState('')
 
     const ratings = async () => [
-        {label: '*', value: 1},
-        {label: '**', value: 2},
-        {label: '***', value: 3},
-        {label: '****', value: 4},
-        {label: '*****', value: 5}
+        {label: '1', value: 1},
+        {label: '2', value: 2},
+        {label: '3', value: 3},
+        {label: '4', value: 4},
+        {label: '5', value: 5}
     ]
 
     // ** local States
