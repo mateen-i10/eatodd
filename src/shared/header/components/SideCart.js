0e65dd1f6332e4aaace048f4a44c2e58173cfa99
Shahzad-Dev <m.shahzad4477@gmail.com>
2023-02-03 11:14:50 +0500

    Show guest Name with meal order in side cart in group Order

--- src/shared/header/components/SideCart.js
+++ src/shared/header/components/SideCart.js
@@ -56,7 +56,7 @@ const Cart = (props) => {
         (!cartItems.wines || (cartItems.wines && cartItems.wines.length === 0)) &&
         (!cartItems.catering || (cartItems.catering && cartItems.catering.length === 0)))
 
-    console.log('cartItems', cartItems)
+    console.log('cartItems-----------------', getCartData())
 
     useEffect(() => {
         setCartItems({...getCartData()})
