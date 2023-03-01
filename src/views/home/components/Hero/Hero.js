086429dd0c59f780789480d907e38aa0ae3b673c
zabair <zabair1146@gmail.com>
2023-03-01 19:08:45 +0500

    next and previous icon on slider

--- src/views/home/components/Hero/Hero.js
+++ src/views/home/components/Hero/Hero.js
@@ -35,12 +35,12 @@ class Hero extends Component {
                     </div>
                     <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample"
                             data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true" style={{fontSize: 50}}></span>
+                        <span className="carousel-control-prev-icon" aria-hidden="true" style={{fontSize: 50, height: '50px', width: '50px'}}></span>
                         <span className="visually-hidden">Previous</span>
                     </button>
                     <button className="carousel-control-next" type="button" data-bs-target="#carouselExample"
                             data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="carousel-control-next-icon" aria-hidden="true" style={{height: '50px', width: '50px'}}></span>
                         <span className="visually-hidden">Next</span>
                     </button>
                 </div>
