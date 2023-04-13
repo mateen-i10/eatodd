48463a11e8877c36afcf35a33dd49617bddfa4c9
zabair <zabair1146@gmail.com>
2023-04-13 16:26:56 +0500

    check out button

--- src/shared/header/Header.js
+++ src/shared/header/Header.js
@@ -8,10 +8,10 @@ import {ShoppingCart} from "react-feather"
 import UserDropdown from "../../@core/layouts/components/navbar/UserDropdown"
 import {useDispatch, useSelector} from "react-redux"
 import {Button} from "reactstrap"
-import {isCustomer, isUserLoggedIn} from "../../auth/utils"
+import {getUserData, isCustomer, isUserLoggedIn} from "../../auth/utils"
 import {scrollToOrderAdded} from "../../redux/scroll/scrollSlice"
-import {isJoinedByLink} from "../../utility/Utils"
-import checkOut from "../../views/checkoutpage/CheckOut"
+import {cartTotalPrice, isJoinedByLink} from "../../utility/Utils"
+import {toast} from "react-toastify"
 
 export default function Header({isSimple, setKey}) {
     const totalItems = useSelector(state => state.cartItems.total)
@@ -38,6 +38,14 @@ export default function Header({isSimple, setKey}) {
     useEffect(() => {
         document.body.classList.toggle('nav-open', isOpen)
     }, [isOpen])
+
+    const checkOut = () => {
+        /*toggleCanvasStart()*/
+        const totalPrice = cartTotalPrice()
+        if (!getUserData() || !isCustomer()) history.push('/login', {returnURL: '/checkout'})
+        else if (!totalPrice || totalPrice <= 0) toast.info(" You have selected add-on items.")
+        else if (totalPrice && totalPrice > 0) history.push('/checkout')
+    }
 
     const onCateringClick = () => {
         history.push('/gmap', {returnURL: 'catering', isCatering: true})
