57b2c82af511bd15ae06223a4b51fc7aed90707f
mateen <mateen@vu360solutions.org>
2023-04-28 14:31:37 +0500

    Changes in catring

--- src/views/catering/Pages/Catering.js
+++ src/views/catering/Pages/Catering.js
@@ -47,8 +47,6 @@ const Catering = () => {
             })
             setCateringMenu(final)
         }
-
-
     }, [response])
 
 
@@ -59,65 +57,46 @@ const Catering = () => {
 
     // console.log(cateringMenu)
 
-    /*const xl = "6"
-    const md = "6"*/
-
- /*   const toggleList = item => {
-        // console.log("Item", item)
-        if (selectedMenuId !== item.id) {
-            setSelectedMenuId(item.id)
-        }
-        if (item.isWine) setWineSelected(true)
-    }*/
-
     console.log("selectedMenuId", selectedMenuId)
 
     return (
         <>
             <Header/>
 
-            <div style={{backgroundImage: `url(${luxurayWine})`, backgroundSize: '1200px'}}>
-                <section>
-                    <div className="continer-sm" style={{backgroundColor: 'rgba(0, 13, 26, .5)'}}>
-                        <h5 style={{color:'white', textAlign: 'center', paddingTop: '50px', fontSize: '2.8em', textTransform: 'uppercase'}}>OMG Catering</h5>
-                        <h4 style={{color:'white', textAlign: 'center', fontSize: '2.2em', paddingTop:'30px', paddingBottom: '30px', textTransform: 'uppercase'}}>A Warm, Charming Atmosphere</h4>
-                        <p style={{color:'white', textAlign: 'center', fontSize: '1.5em', paddingBottom:'70px', marginTop: '-30px'}}>Allow us to make your next special event extra special. We cater for all sized<br />functions, ideal for your larger functions or an intimate gathering, our team can<br />curate a menu to suit your taste.</p>
-                    </div>
-                </section>
+            <div style={{backgroundImage: `url(${luxurayWine})`, height: '300px', backgroundSize: '100%'}}>
             </div>
+            <section style={{paddingTop:'50px', paddingBottom: '50px'}}>
+                <div className="continer-sm">
+                    <h5 style={{
+                        textAlign: 'center',
+                        paddingTop: '50px',
+                        fontSize: '2.8em',
+                        textTransform: 'uppercase',
+                        fontFamily: 'Gotham Bold',
+                        color: 'black'
+                    }}>OMG Catering</h5>
+                    <h4 style={{
+                        textAlign: 'center',
+                        fontSize: '2.2em',
+                        paddingTop: '10px',
+                        paddingBottom: '30px',
+                        textTransform: 'uppercase'
+                    }}>A Warm, Charming Atmosphere</h4>
+                    <p style={{textAlign: 'center', fontSize: '1.5em', paddingBottom: '50px', marginTop: '-30px'}}>Allow
+                        us to make your next special event extra special. We cater for all sized<br/>functions, ideal
+                        for your larger functions or an intimate gathering, our team can<br/>curate a menu to suit your
+                        taste.</p>
+                </div>
+            </section>
 
             <section>
-                <div className="fs-1 fw-bolder text-primary text-center" style={{paddingTop:"50px", paddingBottom:"20px"}}>OMG CATERING PACKAGES
+                <div className="fs-1 fw-bolder text-primary text-center"
+                     style={{paddingTop: "50px", paddingBottom: "20px"}}>OMG CATERING PACKAGES
                 </div>
-            <p style={{textAlign: 'center', fontSize: '1.5em', paddingBottom:'70px'}}>Treat your group to Taste & Flavor that happens to be Healthy!<br />*24 hour advance notice requested</p>
+                <p style={{textAlign: 'center', fontSize: '1.5em', paddingBottom: '70px'}}>Treat your group to Taste &
+                    Flavor that happens to be Healthy!<br/>*24 hour advance notice requested</p>
                 {cateringMenu.length ? <div className="container-sm mb-3">
                     <div className="row mt-3">
-                        {/*<div className="col-md-3 col-9 mx-auto mb-3" style={{marginLeft: '-50px'}}>
-                        <div className="text-center fs-2 fw-bolder text-primary">Menu</div>
-                        <hr/>
-                        <div
-                            style={{
-                                borderRight: "6px solid rgb(129 190 65)"
-                            }}
-                        >
-                            {cateringMenu.map((item, i) => (
-                                <div key={i}
-                                     className={`fs-3 fw-bolder ms-2 cursor-pointer mb-1  ${elHovered[i] ? "text-primary" : ""}  `}
-                                     style={{lineHeight: "35px"}}
-                                     onMouseOver={() => (setElHovered({...elHovered, [i]: true}))}
-                                     onMouseLeave={() => (setElHovered({...elHovered, [i]: false}))}
-                                     onLoad={() => setSelectedMenuId(item.id)}
-                                     onClick={() => {
-                                         toggleList(item)
-                                     }}
-                                >
-                                    <div
-                                        className={`text-start text-uppercase ${selectedMenuId === item.id ? "text-primary" : ""}`}
-                                        style={{fontSize: 15}}>{item.name.toLowerCase() === 'home' ? null : item.name}</div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>*/}
                         <div className="col-md-12">
                             {selectedMenuId === 0 && isWineSelected ? <WineOrderMenu/> : <CateringDetailMenu
                                 /*xl={xl}
@@ -129,53 +108,57 @@ const Catering = () => {
                     </div>
                     <Row style={{textAlign: "center"}}>
                         <Col md={12}>
-                            <div className="fs-1 fw-bolder text-primary text-center" style={{paddingTop:"50px", paddingBottom:"20px"}}>OMG GRAZING TABLE
+                            <div className="fs-1 fw-bolder text-primary text-center"
+                                 style={{paddingTop: "50px", paddingBottom: "20px"}}>OMG GRAZING TABLE
                             </div>
-                            <p style={{textAlign: 'center', fontSize: '1.5em', paddingBottom:'70px'}}>CATERING PACKAGE SERVES 25+ PEOPLE</p>
+                            <p style={{textAlign: 'center', fontSize: '1.5em', paddingBottom: '70px'}}>CATERING PACKAGE
+                                SERVES 25+ PEOPLE</p>
                         </Col>
                     </Row>
                     <Row>
                         <Col lg={4}></Col>
                         <Col lg={4} style={{textAlign: "center"}}>
-                            <button className='btn btn-primary mt-2 mb-2' onClick={() => setFormComponent(!formComponent)}>Show More</button>
+                            <button className='btn btn-primary mt-2 mb-2'
+                                    onClick={() => setFormComponent(!formComponent)}>Show More
+                            </button>
                         </Col>
                         <Col lg={4}></Col>
                     </Row>
                     {formComponent ? <>
-                    <Row>
-                        <Col lg={2}></Col>
-                        <Col lg={8}>
-                            <CateringSubForm/>
-                        </Col>
-                        <Col lg={2}></Col>
-                    </Row>
+                        <Row>
+                            <Col lg={2}></Col>
+                            <Col lg={8}>
+                                <CateringSubForm/>
+                            </Col>
+                            <Col lg={2}></Col>
+                        </Row>
                     </> : ''}
-
                     {/*{selectedMenuId === '1' ? <CateringSubForm/> : []}*/}
                 </div> : <div className="m-5"><ComponentSpinner/></div>}
             </section>
 
             <section className="container-sm" style={{backgroundColor: '#f5f8fb'}}>
                 <Row>
-                    <Col lg={7} style={{paddingBottom:'50px'}}>
+                    <Col lg={7} style={{paddingBottom: '50px'}}>
                         <div className='ps-5 mt-5'>
                             <p className="text-primary fs-2 fw-bolder">
                                 OMG Catering
                             </p>
-                            <h1 className="text-black text-uppercase fw-bolder mb-2" style={{fontSize: '40px', lineHeight: 1.1}}>
+                            <h1 className="text-black text-uppercase fw-bolder mb-2"
+                                style={{fontSize: '40px', lineHeight: 1.1}}>
                                 ADD WINE TO YOUR <br/>OMG CATERING ORDER
                             </h1>
                             <div className="fs-4 mb-2">
                                 <p>
-                                    OMG will streamline your event planning by taking care of your food + WINE. We offer the
+                                    OMG will streamline your event planning by taking care of your food + WINE. We offer
+                                    the
                                     ability to Mix and Match your wine selection to make sure no guest leaves thirsty.
                                 </p>
                                 <div className="mb-2" style={{fontSize: "15px"}}>
                                     *All wine will be sold at wholesale prices
                                     <br/>
-                                    *$120 service fee added to all event orders that include wine
-                                    <br/>
-                                    *please provide 48-hour notice so we can make sure we have your selected wines in stock
+                                    *Please provide 1 week advance notice so we can make sure we have your selected
+                                    wines in stock
                                 </div>
                             </div>
                             <button type="button" className="btn fs-4 mt-3 mb-2" onClick={() => setWineShow(!wineShow)}
--- src/views/home/components/Hero/Hero.js
+++ src/views/home/components/Hero/Hero.js
@@ -1,59 +1,121 @@
-import React, {Component} from 'react'
-import "./Hero.css"
-// import {Link} from "react-router-dom"
+import React from 'react'
+import './Hero.css'
+import {useHistory} from "react-router-dom"
 
-class Hero extends Component {
-    render() {
-        return (
-            <div className="hero-container">
-                <div id="carouselExample" className="carousel slide banner">
-                    <div>
-                        <div className="carousel-item active">
-                            <div style={{backgroundColor: "black", width: "100%", height: "100%", position: "absolute", margin: "auto", opacity: "30%"}}></div>
-                            <div className='banner-content' style={{position: "absolute", zIndex: 1}}>
-                                <h1 className='banner-text' style={{color:"#000000"}}>OMG Offers Bottles of Wine With Zero Mark Up</h1>
-                                <h4 style={{color:"white"}}>Sign Up for the omg wine club and start saving on your favourite bottles TODAY</h4>
-                                <div className="btn btn-danger" style={{zIndex: 1}}>JOIN OMG WINE CLUB</div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div style={{backgroundColor: "black", width: "100%", height: "100%", position: "absolute", margin: "auto", opacity: "30%"}}></div>
-                                <div className='banner-content' style={{position: "absolute", zIndex: 1}}>
-                                <h1 className='banner-text' style={{color:"#000000"}}>Treat your Co Workers, Family and Friends To Omg Catering!</h1>
-                                <h4 style={{color:"white"}}>Serving groups of 4-400, every bite and sip will leave your guests saying OMG.</h4>
-                                <div className="btn btn-danger" style={{zIndex: 1}} onClick={() => console.log("works")}>Order Catering Now</div>
-                            </div>
-                        </div>
-                        <div className="carousel-item">
-                            <div style={{backgroundColor: "black", width: "100%", height: "100%", margin: "auto", opacity: "30%"}}></div>
-                            <div className='banner-content'>
-                               <h1 style={{color:"#000000", fontSize: 70}} className='banner-text'>OMG</h1>
-                               <h2 style={{color:"white", fontSize: 70}}>Eat, Love, Live</h2>
-                               <div className="btn btn-danger" style={{zIndex: 1}}>Order Now</div>
+const Hero = () => {
+    const history = useHistory()
+    const onCateringClick = () => {
+        history.push('/gmap', {returnURL: 'catering', isCatering: true})
+    }
+    return (
+        <div className="hero-container">
+            <div id="carouselExample" className="carousel slide banner">
+                <div>
+                    <div className="carousel-item active">
+                        <div
+                            style={{
+                                backgroundColor: 'black',
+                                width: '100%',
+                                height: '100%',
+                                position: 'absolute',
+                                margin: 'auto',
+                                opacity: '30%'
+                            }}
+                        ></div>
+                        <div className="banner-content" style={{position: 'absolute', zIndex: 1}}>
+                            <h1 className="banner-text" style={{color: '#000000', fontWeight: '700'}}>
+                                OMG OFFERS BOTTLES OF WINE WITH ZERO MARK UP
+                            </h1>
+                            <h3 style={{color: 'white'}}>
+                                Sign Up for the omg wine club and start saving on your Favorite bottles Today
+                            </h3>
+                            <div
+                                className="btn btn-danger"
+                                style={{zIndex: 1}}
+                                onClick={() => history.push('/wine/membership')}
+                            >
+                                JOIN OMG WINE CLUB
                             </div>
                         </div>
                     </div>
-                    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample"
-                            data-bs-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true" style={{fontSize: 50, height: '50px', width: '50px'}}></span>
-                        <span className="visually-hidden">Previous</span>
-                    </button>
-                    <button className="carousel-control-next" type="button" data-bs-target="#carouselExample"
-                            data-bs-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true" style={{height: '50px', width: '50px'}}></span>
-                        <span className="visually-hidden">Next</span>
-                    </button>
+                    <div className="carousel-item">
+                        <div
+                            style={{
+                                backgroundColor: 'black',
+                                width: '100%',
+                                height: '100%',
+                                position: 'absolute',
+                                margin: 'auto',
+                                opacity: '30%'
+                            }}
+                        ></div>
+                        <div className="banner-content" style={{position: 'absolute', zIndex: 1}}>
+                            <h1 className="banner-text" style={{color: '#000000', fontWeight: '700'}}>
+                                TREAT YOUR CO WORKERS, FAMILY AND FRIENDS TO OMG CATERING!
+                            </h1>
+                            <h3 style={{color: 'white'}}>
+                                Serving groups of 4-400, every bite and sip will leave your guests saying OMG.
+                            </h3>
+                            <div className="btn btn-danger" style={{zIndex: 1}} onClick={onCateringClick}>
+                                Order Catering Now
+                            </div>
+                        </div>
+                    </div>
+                    <div className="carousel-item">
+                        <div
+                            style={{
+                                backgroundColor: 'black',
+                                width: '100%',
+                                height: '100%',
+                                margin: 'auto',
+                                opacity: '30%'
+                            }}
+                        ></div>
+                        <div className="banner-content">
+                            <h1 style={{color: '#000000', fontSize: 70, fontWeight: '700'}} className="banner-text">
+                                OMG
+                            </h1>
+                            <h2 style={{color: 'white', fontSize: 70}}>Eat, Love, Live</h2>
+                            <div className="btn btn-danger" style={{zIndex: 1}} onClick={() => history.push('#orderSection')}>
+                                Order Now
+                            </div>
+                        </div>
+                    </div>
                 </div>
-
-                <video autoPlay="autoplay" muted="muted"
-                         loop="loop"
-                         src={'https://remote.vu360solutions.org/static/eatomg.mp4'}
-                         className="background-video" />
+                <button
+                    className="carousel-control-prev"
+                    type="button"
+                    data-bs-target="#carouselExample"
+                    data-bs-slide="prev"
+                >
+          <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+              style={{fontSize: 50, height: '50px', width: '50px'}}
+          ></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next"
+                    type="button"
+                    data-bs-target="#carouselExample"
+                    data-bs-slide="next"
+                >
+                    <span className="carousel-control-next-icon" aria-hidden="true"
+                          style={{height: '50px', width: '50px'}}></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
-        )
-    }
+            <video
+                autoPlay="autoplay"
+                muted="muted"
+                loop="loop"
+                src={'https://remote.vu360solutions.org/static/eatomg.mp4'}
+                className="background-video"
+            />
+        </div>
+    )
 }
-
 
 export default Hero
 
