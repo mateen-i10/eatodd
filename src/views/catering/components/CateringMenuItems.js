18eb12c08e2e004c65f4c5952a786669aa8e2822
mateen <mateen@vu360solutions.org>
2023-07-10 12:16:48 +0500

    fixed limit issue , packages

--- src/views/catering/components/CateringMenuItems.js
+++ src/views/catering/components/CateringMenuItems.js
@@ -1,51 +1,40 @@
 import React from 'react'
 import { Button, Col, Row } from "reactstrap"
 import { Link } from "react-router-dom"
-import cardImage from "../../../assets/images/pages/wine/about/Layer-456.png"
 
 const CateringMenuItems = (props) => {
     const { item } = props
 
     return (
-        <div className="container-sm" style={{ backgroundColor: "#f4f6f9" }}>
-            <div className="row align-items-center justify-content-center" style={{ marginTop: '0px !important' }}>
-                <div className="col-md-12 mt-5">
-                    <div className="member-card card border" style={{ width: "100%", height: "550px" }}>
+        <div className="member-card card border" style={{ width: "100%", height: "550px" }}>
                         <div className='mt-3'>
-                            <h2 className="fw-bolder text-center text-primary mb-2" style={{ fontSize: '20px', textTransform: 'capitalize' }}>
+                            <h2 className="fw-bolder text-center text-primary mb-2" style={{ fontSize: '20px' }}>
                                 {item.title.toUpperCase()}
                             </h2>
-                        </div>
-
-                        <div className="card-body align-items-center justify-content-center">
-                            <Row>
-                                <Col lg={12} className='text-center'>
-                                    <img className='imgCenter imgStyle' width="25%" src={cardImage} height="auto" alt="standard-plan-img" />
-                                </Col>
-                            </Row>
-                            <h5 className="text-capitalize text-center mt-3" style={{ fontSize: '16px', fontWeight: '500' }}>
-                                {item.detail}
-                            </h5>
-                            <div className="mt-1 fw-bolder text-center amountMargin">
+                            <div className="fw-bolder text-center amountMargin">
                                 <sup className="fs-4 dollarSign text-primary"></sup>
-                                <span className="text-primary" style={{ fontSize: "2rem" }}>${item.price}</span>
+                                <span className="text-primary" style={{ fontSize: "3rem" }}>${item.price}</span>
                                 {item.price !== 0 && (
                                     <>
                                         <span className="fw-bolder text-primary text-center"> / </span>
-                                        <span className="fw-bolder text-primary text-center">persons</span>
-                                        <Link to={`/cateringMenuOrder/${item.id}`}>
-                                            <Button className="text-uppercase" color="primary" style={{ width: "150px" }}>
-                                                Select
-                                            </Button>
-                                        </Link>
+                                        <span className="fw-bolder text-primary text-center">persons</span><br></br>
                                     </>
                                 )}
                             </div>
                         </div>
+                        <div className="card-body align-items-center justify-content-center">
+                            <h5 className="text-capitalize text-center mt-3" style={{ fontSize: '16px', fontWeight: '500' }}>
+                                {item.detail}
+                            </h5>
+                        </div>
+                        <div className="card-footer" style={{ margin: 'auto', borderTop:'0px'}}>
+                            <Link to={`/cateringMenuOrder/${item.id}`}>
+                                <Button className="text-uppercase" color="primary" style={{ width: "150px"}}>
+                                    Select
+                                </Button>
+                            </Link>
+                        </div>
                     </div>
-                </div>
-            </div>
-        </div>
     )
 }
 
